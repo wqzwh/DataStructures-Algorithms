85018dae12eef301b676e60155cfc2a6d51b01c0
wqzwh <wangziqihappy@163.com>
2018-12-12 15:29:58 +0800

    feature: 字典树增加contains和isPrefix方法

--- src/Trie/index.js
+++ src/Trie/index.js
@@ -36,5 +36,27 @@ class Trie {
       this.size++
     }
   }
+
+  // 在字典树中查询单词是否包含
+  contains(word) {
+    let cur = this.root
+    for(let i = 0; i < word.length; i++){
+      let c = word.charAt(i)
+      if(cur.next.get(c) === null) return false
+      cur = cur.next.get(c)
+    }
+    return cur.isWord
+  }
+
+  // 在字典树中查询单词是否有以prefix为前缀的
+  isPrefix(prefix) {
+    let cur = this.root
+    for(let i = 0; i < prefix.length; i++){
+      let c = prefix.charAt(i)
+      if(cur.next.get(c) === null) return false
+      cur = cur.next.get(c)
+    }
+    return true
+  }
 }
 
