73c4ff4c19edd491a69bb6e3b5a2eea899e4825d
wqzwh <wangziqihappy@163.com>
2018-12-15 16:57:56 +0800

    feature：基础并查集添加

---
+++ src/UnionFind/index.js
@@ -0,0 +1,40 @@
+/**
+ *
+ * 并查集
+ *
+ */
+const _find = Symbol('_find')
+class UnionFind {
+  constructor(size) {
+    this.id = new Array(size)
+    for(let i = 0; i < id.length; i++) {
+      this.id[i] = i
+    }
+  }
+
+  isConnected(p, q) {
+    return this[_find](p) === this[_find](q)
+  }
+
+  unionElement(p, q) {
+    const PID = this[_find](p)
+    const QID = this[_find](q)
+
+    if(PID === QID) return
+    for(let i = 0; i < this.id.length; i++) {
+      if(this.id[i] === PID) {
+        this.id[i] === QID
+      }
+    }
+  }
+
+  getSize() {
+    return this.id.length
+  }
+
+  // 查找元素p对应的集合编号
+  [_find](p) {
+    if(p < 0 && p >= this.id.length) return '元素不合法'
+    return this.id[p]
+  }
+}
