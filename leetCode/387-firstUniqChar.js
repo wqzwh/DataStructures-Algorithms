d500d4821e4cd32b5183a0620d42262e810e952a
wqzwh <wangziqihappy@163.com>
2018-12-18 22:41:24 +0800

    feature: 添加leetcode中387代码示例

---
+++ leetCode/387-firstUniqChar.js
@@ -0,0 +1,25 @@
+/**
+ *
+ * 387. 字符串中的第一个唯一字符
+ *
+ */
+
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const firstUniqChar = function(s) {
+  const frequency = new Array(26)
+  for(let i = 0; i < s.length; i++) {
+    if(!frequency[s.charCodeAt(i) - 'a'.charCodeAt()]) frequency[s.charCodeAt(i) - 'a'.charCodeAt()] = 0
+    frequency[s.charCodeAt(i) - 'a'.charCodeAt()]++
+  }
+  for(let i = 0; i < s.length; i++) {
+    if(frequency[s.charCodeAt(i) - 'a'.charCodeAt()] === 1) {
+      return i
+      break
+    }
+  }
+  return -1
+};
