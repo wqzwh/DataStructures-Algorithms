b81ca11965c62b5e63088c18969eda866d54d3c1
wqzwh <wangziqihappy@163.com>
2018-12-20 21:06:31 +0800

    feature: 更新代码目录结构，增加webpack相关配置，增加数组类的测试用例

---
+++ src/test/arrayTest.js
@@ -0,0 +1,13 @@
+const CArray = require('../dataStructures/array/index.js')
+
+const aa = new CArray()
+
+for(let i = 0; i < 10; i++){
+  aa.addLast(i)
+}
+aa.addLast(100)
+
+console.log(aa)
+console.log(aa.query(2))
+aa.swap(2,8)
+console.log(aa)
---
+++ webpack.dev.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname,
+    filename: './dist/bundle.js'
+  },
+
+  module: {
+    rules: [{
+      test: /\.js?$/,
+      exclude: '/(node_modules)/',
+      loader: 'babel-loader'
+    }]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
+  ],
+
+  devServer: {
+    contentBase: path.join(__dirname, './dist'),
+    open: true,
+    port: 3000
+  }
+}
